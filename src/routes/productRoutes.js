6b67c6295ff5dd231dd1b1e57148930056597b53
Ommalvankar <ommalvankar55@gmail.com>
2026-08-12 11:12:37 +0530

    Fix typo in import statement for upload middleware

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const { protect } = require("../middleware/authMiddleware");
 const admin = require("../middleware/adminMiddleware");
-cost upload = reqiure("../middleware/upload");
+const upload = reqiure("../middleware/upload");
 const {
     createProduct,
     getAllProducts,

e5acb154593593aa3084c101c6e1e05386950885
Ommalvankar <ommalvankar55@gmail.com>
2026-08-12 09:36:30 +0530

    Change default user role to 'admin'

--- src/models/User.js
+++ src/models/User.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema({
     role: {
         type: String,
         enum: ['customer', 'admin'],
-        default: 'customer'
+        default: 'admin'
     }
 }, { timestamps: true });
 

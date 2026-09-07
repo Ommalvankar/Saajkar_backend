a744770de33eadfab8dddf1d47ff30a376dc320c
Ommalvankar <ommalvankar55@gmail.com>
2026-09-07 12:10:56 +0530

    Refactor product schema for better structure

--- src/models/product.js
+++ src/models/product.js
@@ -1,76 +1,87 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema(
-{
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-
-    description: {
-        type: String,
-        required: true
-    },
-
-    category: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Category",
-    required: true
-},
-    price: {
-        type: Number,
-        required: true
-    },
-
-    discountPrice: {
-        type: Number,
-        default: 0
-    },
-
-    images: [
     {
-        public_id: {
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        description: {
             type: String,
             required: true
         },
-        url: {
-            type: String,
+
+        category: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Category",
             required: true
+        },
+
+        price: {
+            type: Number,
+            required: true
+        },
+
+        discountPrice: {
+            type: Number,
+            default: 0
+        },
+
+        images: [
+            {
+                public_id: {
+                    type: String,
+                    required: true
+                },
+
+                url: {
+                    type: String,
+                    required: true
+                }
+            }
+        ],
+
+        material: {
+            type: String,
+            default: "Handcrafted"
+        },
+
+        stock: {
+            type: Number,
+            default: 0
+        },
+
+        featured: {
+            type: Boolean,
+            default: false
+        },
+
+        // =========================
+        // BEST SELLER
+        // =========================
+        bestSeller: {
+            type: Boolean,
+            default: false
+        },
+
+        status: {
+            type: String,
+            enum: ["Available", "Out of Stock"],
+            default: "Available"
+        },
+
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
         }
+    },
+
+    {
+        timestamps: true
     }
-],
-
-    material: {
-        type: String,
-        default: "Handcrafted"
-    },
-
-    stock: {
-        type: Number,
-        default: 0
-    },
-
-    featured: {
-        type: Boolean,
-        default: false
-    },
-
-    status: {
-        type: String,
-        enum: ["Available", "Out of Stock"],
-        default: "Available"
-    },
-
-    createdBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
-    }
-
-},
-{
-    timestamps: true
-});
+);
 
 module.exports =
     mongoose.models.Product ||
